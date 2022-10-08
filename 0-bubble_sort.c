25ad3ac873d57d2ae09b49fe9405708ef847d543
Alishia Subramoney <subramoneyalishia@gmail.com>
2022-10-08 16:16:56 +0200

    Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,16 +7,24 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-    for (int i = 0; i < size; i++)
+    int i , j, swap, tmp;
+
+    for (i = 0; i < size; i++)
     {
-        for (int j = 0; j < size - i - 1; j++)
+        swap = 0
+
+        for (j = 0; j < size - i - 1; j++)
         {
             if (array[j] > array[j + 1])
             {
-                int swap = array[j];
+                tmp = array[j];
                 array[j] = array[j + 1];
-                array[j + 1] = swap;
+                array[j + 1] = tmp;
+
+                swap = 1;
             }
         }
+        if (swap == 0)
+            break;
     }
 }
